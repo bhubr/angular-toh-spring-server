c97b7f5be3c3e38e6d809e57851f9b0cf3965c93
Benoît Hubert <benoithubert@gmail.com>
2021-06-30 00:45:12 +0200

    Generate with initializr

---
+++ src/main/java/me/benoithubert/angulartoh/AngularTohApplication.java
@@ -0,0 +1,13 @@
+package me.benoithubert.angulartoh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularTohApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularTohApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/benoithubert/angulartoh/AngularTohApplicationTests.java
@@ -0,0 +1,13 @@
+package me.benoithubert.angulartoh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngularTohApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
